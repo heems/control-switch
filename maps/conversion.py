c54c3bdd33f5b62363dd359c398e5ead11fa3228
Michael Mattheakis <michaelm244@gmail.com>
2014-01-25 14:56:05 -0800

    michael clutches it

--- maps/conversion.py
+++ maps/conversion.py
@@ -1,8 +1,10 @@
+import textwrap
+
 filename = raw_input("Filename?\n")
 
 f = open(filename, 'rw')
+text = f.read()
+text = ','.join(textwrap.wrap(text, 1))
+print text
 
 map_array = []
-
-for line in f:
-	print line
