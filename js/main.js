06794f7ec3bca9cb4b90e63f884ea27651c9ac04
Karthik Bala <karthikbala444@gmail.com>
2014-01-25 18:34:17 -0800

    gravity and collision

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ enchant();
 
 window.onload = function(){
 	var game = new Game(640, 480);
+	var GRAVITY = 2;
+	var GRAVITY_ACC = 1.1;
 	game.preload('assets/chara1.png');
 	game.preload('assets/map2.png');
 	console.log(game);
-	game.preload('');
 
 	game.fps = 30;
 	game.scale = 1;
@@ -92,6 +93,8 @@ window.onload = function(){
 		var jumping = false;
 
 		game.addEventListener('enterframe', function(){
+			sprite.y += GRAVITY;
+			GRAVITY *= GRAVITY_ACC;
 			if(!jumping) {
 				if (game.input.up){
 					jumping = true;
@@ -101,9 +104,6 @@ window.onload = function(){
 						});
 					});
 				}
-				if (game.input.down) {
-					sprite.y += 10;
-				}
 			}
 			if (game.input.left){
 				sprite.x -= MOVE_SPEED;
@@ -111,6 +111,11 @@ window.onload = function(){
 
 			if (game.input.right){
 				sprite.x += MOVE_SPEED;
+			}
+			if (backgroundMap.hitTest(sprite.x, sprite.y + sprite.height) === true){
+				GRAVITY = 0;
+			} else if (GRAVITY == 0){
+				GRAVITY = 2;
 			}
 		});
 	};
